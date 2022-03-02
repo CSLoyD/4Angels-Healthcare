848efdb4ebfc5cdc26ae68e743eccdd5b0f12c5d
Alextides <web2.alextides@gmail.com>
2022-03-02 16:39:58 +0800

    up

--- src/modules/home/components/Menu.js
+++ src/modules/home/components/Menu.js
@@ -34,7 +34,7 @@ const Menu = (props) => {
                                         </Body>
                                     </CardItem>
                                 </TouchableOpacity> */}
-                                <TouchableOpacity style={{width: 150}} activeOpacity={1} onPress={() => navigation.navigate('Shifts') }>
+                                <TouchableOpacity style={{width: 150}} activeOpacity={1} onPress={() => console('Shifts') }>
                                     <CardItem style={{backgroundColor: '#0086b3', height: 150, borderTopRightRadius: 20, borderTopLeftRadius: 20, borderBottomRightRadius: 20, borderBottomLeftRadius: 20}}>
                                         <Body style={styles.container}>
                                             <Text>
